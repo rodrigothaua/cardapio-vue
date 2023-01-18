3d8397da7df25904a4ac9611b53ee4d5057e278d
Rodrigo Thauã Lima Barroso <rodrigothaua@gmail.com>
2023-01-18 15:54:01 -0400

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/Header.vue';
 
 export default {
-  name: 'App',
+  name: "index",
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+    color: rgb(27, 28, 30);
+  }
+
+  body{
+    -webkit-font-smoothing: antialiased;
+    color: rgb(27, 28, 30);
+    background: f1f1f1;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header id="cabecalho">
+        <div class="menu-icon">
+            <a data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                <i class="bi bi-list"></i>
+            </a>
+        </div>
+
+        <div class="menu-logo">
+            LOGO
+        </div>
+
+        <div class="menu-right">
+            <a href="login.html">
+                <i class="bi bi-person"></i>
+            </a>
+        </div>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+    
+<style>
+    #cabecalho{
+        height: 6rem;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        box-shadow: rgb(0 0 0 / 10%) 0px 4px 16px 0px;
+        background: rgb(248, 248, 248);
+        width: 100%;
+        padding: 0 1.8rem 0 1.8rem;
+    }
+
+    .menu-logo{
+        justify-content: center;
+        font-size: 30px;
+    }
+
+    .menu-right{
+        justify-content: flex-end;
+    }
+
+    .offcanvas-body{
+        background-color: #000;
+    }
+</style>
